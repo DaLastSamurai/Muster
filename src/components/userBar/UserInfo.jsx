51d547b6e15263f332ee9a72ec75f9855e4aacfa
Christine <christinehma214@gmail.com>
2017-12-02 15:58:22 -0500

    profile links up with sidebar button

--- src/components/userBar/UserInfo.jsx
+++ src/components/userBar/UserInfo.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 import LinkButton from '../helperElements/LinkButton'
 // need to change so that clicking on takes you to the profile page of the user
+import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom'
+
 
 export default class UserInfo extends React.Component {
   constructor(props){
     super(props);
   }
-  render() { 
-    // console.log('this is the uid', this.props.user.uid)
+
+  render() {
+    console.log('this is the uid', this.props.user.uid)
     return (
-      <div> 
+      <div>
+        {/*<Link to={`/profile/${this.props.user.uid}`}></Link>
         <img src="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-128.png" />
-        {this.props.user.displayName} 
+        {this.props.user.displayName}*/}
+        <Link to={`/profile/${this.props.user.uid}`}>
+          <LinkButton title={this.props.user.displayName}/>
+        </Link>
       </div>
     )
   }
