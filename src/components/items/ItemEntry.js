9cfac49dff81e0cd5761b60c1c2b70f960f8ca5b
sohee Lim <limsohee1002@gmail.com>
2017-12-04 16:26:08 -0500

    user athentication works

--- src/components/items/ItemEntry.js
+++ src/components/items/ItemEntry.js
@@ -10,7 +10,7 @@ class ItemEntry extends React.Component {
       <div>
         <img src={this.props.item.imageUrl ?  this.props.item.imageUrl : 'https://crestaproject.com/demo/nucleare-pro/wp-content/themes/nucleare-pro/images/no-image-box.png'} /> 
         <h4>{this.props.item.name}</h4>
-        <button onClick={() => this.props.deleteItem(this.props.id)} type="button">Click Me!</button>
+        <button onClick={() => this.props.checkUser(this.props.id, this.props.item.uId)} type="button">Click Me!</button>
       </div>
     )
   }
