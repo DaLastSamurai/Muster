ec9ba87fac3632233139b3f9077938160504e89c
Christine <christinehma214@gmail.com>
2017-12-08 14:07:12 -0500

    dkajf;ke

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ import AddItems from './addItems/addItems';
 import {InstantSearch, SearchBox, Hits, Highlight} from 'react-instantsearch/dom';
 import { Search } from './helperElements/Search.jsx'
 import MessageFrame from './messaging/MessageFrame'
-import { addToFavCat } from './popularcategory/PopCatHelper.jsx'
+import { addToFavCat, removeFromFavCat } from './popularcategory/PopCatHelper.jsx'
 
 export default class App extends React.Component {
   constructor() {
@@ -34,10 +34,11 @@ export default class App extends React.Component {
     };
 
     this.checkAuthStatus = checkAuthStatus.bind(this);
-    this.setIsOnAuthFrame = this.setIsOnAuthFrame.bind(this); 
-    this.reloadPage = this.reloadPage.bind(this); 
+    this.setIsOnAuthFrame = this.setIsOnAuthFrame.bind(this);
+    this.reloadPage = this.reloadPage.bind(this);
     this.getPopularCategory = this.getPopularCategory.bind(this);
     this.addToFavCat = addToFavCat.bind(this);
+    this.removeFromFavCat = removeFromFavCat.bind(this);
   }
 
   componentDidMount() {
@@ -121,7 +122,7 @@ export default class App extends React.Component {
                 <Route exact path='/' render={() =>
                   this.state.isOnAuthFrame
                   ? (<div />)
-                  : <PopularCategoryList popularCategoryList={this.state.popularCategoryList} addToFavCat={this.addToFavCat} />
+                  : <PopularCategoryList popularCategoryList={this.state.popularCategoryList} addToFavCat={this.addToFavCat} removeFromFavCat={this.removeFromFavCat} />
                   }
                 />
                 <Route path='/profile/:uid' onEnter={() => {this.reloadPage()}} component={ProfileFrame} />
--- src/components/popularcategory/PopularCategoryEntry.js
+++ src/components/popularcategory/PopularCategoryEntry.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
+import firebase from 'firebase';
 
 class PopularCategoryEntry extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+
+    }
+    this.handleLike = this.handleLike.bind(this);
+    this.handleDislike = this.handleDislike.bind(this);
+    this.checkIfFavCatContains = this.checkIfFavCatContains.bind(this);
   }
+
+  handleLike(e) {
+    this.props.addToFavCat(e.target.value);
+    this.state[e.target.value] = !this.state[e.target.value];
+    console.log('like2',this.state[e.target.value])
+  }
+
+  handleDislike(e) {
+    this.props.removeFromFavCat(e.target.value);
+    // console.log('HANDLE DISLIKE 1111111111',this.state[e.target.value])
+    this.state[e.target.value] = !this.state[e.target.value];
+    console.log('HANDLE DISLIKE 22222222222',this.state[e.target.value])
+  }
+
+  checkIfFavCatContains(key) {
+    // console.log('111111',key) //key is popular category
+    let currentUID = firebase.auth().currentUser.uid;
+    return firebase.database().ref("users/" + currentUID + "/profileInfo/favoriteCategories")
+      .on('value', (snap) => {
+      // console.log('this is the snap.val', Object.keys(snap.val()), 'this is the key: ', key)
+      var isFavorite = (Object.values(snap.val()).includes(key)); //my fav categories includes(key)?
+      // console.log(isFavorite, 'popcatkey', key, 'myfavcat', snap.val())
+      })
+  }
+
   render() {
+    // console.log(this.state)
+    this.checkIfFavCatContains(this.props.id) //this should set state
     return(
       <div className='popular-category' key={this.props.id}>
+      <Link to={`/collections/:${this.props.category[0]}`}>
         <img src={this.props.category[1].pictureurl}/>
         <h4>{this.props.category[0]}</h4>
+        </Link>
+        <div>
+          {(this.state[this.props.id])
+          ?(<button value={this.props.id} onClick={this.handleDislike}>Unlike</button>)
+          :(<button value={this.props.id} onClick={this.handleLike}>Like!</button>)
+          }
+        </div>
       </div>
     )
   }
