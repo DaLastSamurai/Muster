a42c7b4e3ee1d873e73050683dc8bf5dc7ca5562
sohee Lim <limsohee1002@gmail.com>
2017-12-08 11:19:50 -0500

    connecting with db

--- src/components/manageInventory/InventoryCollection.js
+++ src/components/manageInventory/InventoryCollection.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Dragula from 'react-dragula';
+import firebase from 'firebase';
+import { firebaseAuth, rootRef, collection, category, item, users} from '../../../config/firebaseCredentials';
 import InventoryCollectionList from './InventoryCollectionList';
 
 class InventoryCollection extends React.Component {
@@ -16,13 +19,34 @@ class InventoryCollection extends React.Component {
   }
 
   dragulaDecorator(componentBackingInstance){
-    let drake = Dragula(componentBackingInstance).on('drop', function() {
-      console.log('drop', drake.target)
-      
-    })
-    console.log('sss',this.drake)
-    drake.start(componentBackingInstance)
-  
+    let option = {
+      isContainer: function (el) {
+        return false; // only elements in drake.containers will be taken into account 
+      },
+      moves: function (el, source, handle, sibling) {
+        return true; // elements are always draggable by default 
+      },
+      accepts: function (el, target, source, sibling) {
+        return true; // elements can be dropped in any of the `containers` by default 
+      },
+      invalid: function (el, handle) {
+        return false; // don't prevent any drags from initiating by default 
+      },
+      copy: false                       // elements are moved by default, not copied 
+    // copySortSource: false,             // elements in copy-source containers can be reordered 
+    // revertOnSpill: true,              // spilling will put the element back where it was dragged from, if this is true 
+    // removeOnSpill: false,              // spilling will `.remove` the element, if this is true 
+    // mirrorContainer: undefined    // set the element that gets mirror elements appended 
+    // ignoreInputTextSelection: true     // allows users to select input text, see details below 
+    };
+    if(componentBackingInstance) {
+      Dragula(componentBackingInstance)
+      .on('drop', function(el, target, source) {
+        console.log('drop', 'clicked el', el.className,'moved to ', target.className, 'coming from', source.className)
+        item.child(el.className).child
+      })
+    }
+    
   };
 
   getNodes(n) {
@@ -32,6 +56,7 @@ class InventoryCollection extends React.Component {
   shouldComponentUpdate(nextState) {
     if(this.State !== nextState) {
       this.dragulaDecorator(this.state.node)
+      console.log('nodess', this.state.node)
       return true;
     }
     return false;
@@ -42,7 +67,12 @@ class InventoryCollection extends React.Component {
       <div>
         <h4>sort by collection</h4>
         {Object.keys(this.props.collectionList).map((colKey) => {
-          return <InventoryCollectionList key={colKey} collection={this.props.collectionList[colKey]} itemList={this.props.itemList} getNodes={this.getNodes} />
+          return <InventoryCollectionList 
+            key={colKey} 
+            collectionId={colKey}
+            collection={this.props.collectionList[colKey]} 
+            itemList={this.props.itemList} 
+            getNodes={this.getNodes} />
         })}
       </div>
     )
