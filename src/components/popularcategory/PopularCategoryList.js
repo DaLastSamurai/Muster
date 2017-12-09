4d6ae7b946e088c90e8700a34b483acfda59f770
Seamus Martin <spmartin823@gmail.com>
2017-12-09 10:05:35 -0500

    fixed unauthed home render.

--- src/components/popularcategory/PopularCategoryList.js
+++ src/components/popularcategory/PopularCategoryList.js
@@ -20,7 +20,7 @@ export default class PopularCategoryList extends React.Component {
   }
 
   render() {
-    // console.log('these are the props in PopularCategoryList',this.props)
+    console.log('these are the props in PopularCategoryList',this.props)
     return(
       <div style={{display: 'inline-block'}} className="container-fluid">
         <h2>Popular Categories</h2>
