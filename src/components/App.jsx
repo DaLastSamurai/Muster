7eb1440f89be73587bfca217e90d14a40908237f
sohee Lim <limsohee1002@gmail.com>
2017-12-09 10:07:37 -0500

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import firebase from 'firebase';
 import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
+import firebase from 'firebase';
 import { firebaseAuth, rootRef, collection, category, item, users} from '../../config/firebaseCredentials';
+import { InstantSearch, SearchBox, Hits, Highlight, Pagination } from 'react-instantsearch/dom';
+//react component
 import UnprotectedNav from './nav/UnprotectedNav';
 import ProtectedNav from './nav/ProtectedNav';
 import PopularCategoryList from './popularcategory/PopularCategoryList';
@@ -10,16 +12,13 @@ import AuthFrame from './authentication/AuthFrame';
 import CollectionList from './collections/CollectionList';
 import ItemList from './items/ItemList';
 import ManageInventory from './manageInventory/ManageInventory';
-
-import { checkAuthStatus } from './authentication/authenticationHelpers';
-import ProfileFrame from './profilePages/ProfileFrame';
-import UserInfo from './userBar/UserInfo.jsx'
+import MessageFrame from './messaging/MessageFrame';
 import AddItems from './addItems/addItems';
-
-import { InstantSearch, SearchBox, Hits, Highlight, Pagination } from 'react-instantsearch/dom';
-import { Search } from './helperElements/Search.jsx'
-import MessageFrame from './messaging/MessageFrame'
-
+import UserInfo from './userBar/UserInfo.jsx';
+import ProfileFrame from './profilePages/ProfileFrame';
+import { Search } from './helperElements/Search.jsx';
+//helper
+import { checkAuthStatus } from './authentication/authenticationHelpers';
 
 export default class App extends React.Component {
   constructor() {
@@ -46,32 +45,32 @@ export default class App extends React.Component {
     this.getPopularCategory()
   }
 
-  getPopularCategory() {
-    new Promise((resolve, reject) => {
-      category.on('value', (snap) => {
-        return resolve(snap.val())
-      })
-    })
-    .then((categoryObj) => {
-      var arr = [];
-      Object.keys(categoryObj).forEach((key) => {
-        var tempPromise = new Promise((resolve, reject) => {
-          let collectionCount = Object.keys(categoryObj[key]['collectionId']).length
-          resolve([key, categoryObj[key], collectionCount])
-        })
-        arr.push(tempPromise);
-      })
-      return Promise.all(arr);
-    })
-    .then((data) => {
-      return data.sort((a, b) => {
-        return b[2] - a[2];
-      })
-    })
-    .then(data => {
-      this.setState({popularCategoryList: data})
-    })
-  }
+  // getPopularCategory() {
+  //   new Promise((resolve, reject) => {
+  //     category.on('value', (snap) => {
+  //       return resolve(snap.val())
+  //     })
+  //   })
+  //   .then((categoryObj) => {
+  //     var arr = [];
+  //     Object.keys(categoryObj).forEach((key) => {
+  //       var tempPromise = new Promise((resolve, reject) => {
+  //         let collectionCount = Object.keys(categoryObj[key]['collectionId']).length
+  //         resolve([key, categoryObj[key], collectionCount])
+  //       })
+  //       arr.push(tempPromise);
+  //     })
+  //     return Promise.all(arr);
+  //   })
+  //   .then((data) => {
+  //     return data.sort((a, b) => {
+  //       return b[2] - a[2];
+  //     })
+  //   })
+  //   .then(data => {
+  //     this.setState({popularCategoryList: data})
+  //   })
+  // }
 
   setIsOnAuthFrame(isOnAuthFrame) { this.setState({isOnAuthFrame}) }
 
