489368c3ad3c4e2bacd10dcc88218c46126b54c6
Christine <christinehma214@gmail.com>
2017-12-09 20:33:19 -0500

    weird ternary thing works

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 const algoliasearch = require('algoliasearch');
 const dotenv = require('dotenv'); //configure algolia environment
 const firebase = require('firebase');
-
 // load values from the .env file in this directory into process.env
 dotenv.load();
 
-// configure firebase
+//////////////////////////////////////////// configure firebase
 firebase.initializeApp({
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 const database = firebase.database();
 
-// configure algolia
+//////////////////////////////////////////// install api client / configure algolia
 const algolia = algoliasearch(
   process.env.ALGOLIA_APP_ID,
   process.env.ALGOLIA_API_KEY
 );
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+///////////////////////////////////////////// create my indexes
+const item = algolia.initIndex('item');
+const users  = algolia.initIndex('users')
+const category  = algolia.initIndex('category')
+const collection  = algolia.initIndex('collection')
+////////////////////////////////////////////
 
-index.setSettings({
-  searchableAttributes: [
-    'name',
-    'categories',
-    'users'
-  ]
+// item.setSettings({
+//   searchableAttributes: [
+//     'name',
+//     'categories',
+//     'users'
+//   ]
   // customRanking: ['desc(popularity)'],
-});
-/////////////////////////////////////////////////////////////////////////////
-// One time get allData from Firebase
-// database.ref('item/').once('value', items => {
+// });
+
+///////////////////////////////////////////////////////////////////////////// INitial Import / One time get allData from Firebase
+// database.ref('users/').once('value', item => {
 //   // Build an array of all records to push to Algolia
 //   const records = [];
-//   items.forEach(items => {
+//   item.forEach(item => {
 //     // get the key and data from the snapshot
-//     const childKey = items.key;
-//     const childData = items.val();
+//     const childKey = item.key;
+//     const childData = item.val();
 //     // We set the Algolia objectID as the Firebase .key
 //     childData.objectID = childKey;
 //     // Add object for indexing
 //     records.push(childData);
 //   });
-//
+
 //   // Add or update new objects
-//   index
+//   users
 //     .saveObjects(records)
 //     .then(() => {
 //       console.log('items imported into Algolia');
@@ -52,19 +56,20 @@ index.setSettings({
 //       process.exit(1);
 //     });
 // });
+/////////////////////////////////////////////////////////////////////////////
 
-const items = database.ref('items/');
-items.on('child_added', addOrUpdateIndexRecord);
-items.on('child_changed', addOrUpdateIndexRecord);
-items.on('child_removed', deleteIndexRecord);
+const itemRef = database.ref('item/');
+itemRef.on('child_added', addOrUpdateItemRecord);
+itemRef.on('child_changed', addOrUpdateItemRecord);
+itemRef.on('child_removed', deleteItemRecord);
 
-function addOrUpdateIndexRecord(items) {
+function addOrUpdateItemRecord(items) {
   // Get Firebase object
   const records = items.val();
   // Specify Algolia's objectID using the Firebase object key
   records.objectID = items.key;
   // Add or update object
-  index
+  item
     .saveObject(records)
     .then(() => {
       console.log('Firebase object indexed in Algolia', records.objectID);
@@ -75,17 +80,92 @@ function addOrUpdateIndexRecord(items) {
     });
 }
 
-function deleteIndexRecord(items) {
+function deleteItemRecord(items) {
   // Get Algolia's objectID from the Firebase object key
   const objectID = items.key;
   // Remove the object from Algolia
-  index
+  item
     .deleteObject(objectID)
     .then(() => {
       console.log('Firebase object deleted from Algolia', objectID);
     })
     .catch(error => {
-      console.error('Error when deleting items from Algolia', error);
+      console.error('Error when deleting item from Algolia', error);
       process.exit(1);
     });
 }
+
+const userRef = database.ref('users/');
+userRef.on('child_added', addOrUpdateUserRecord);
+userRef.on('child_changed', addOrUpdateUserRecord);
+userRef.on('child_removed', deleteUserRecord);
+
+function addOrUpdateUserRecord(user) {
+  // Get Firebase object
+  const records = user.val();
+  // Specify Algolia's objectID using the Firebase object key
+  records.objectID = user.key;
+  // Add or update object
+  users
+    .saveObject(records)
+    .then(() => {
+      console.log('Firebase object indexed in Algolia', records.objectID);
+    })
+    .catch(error => {
+      console.error('Error when indexing users into Algolia', error);
+      process.exit(1);
+    });
+}
+
+function deleteUserRecord(user) {
+  // Get Algolia's objectID from the Firebase object key
+  const objectID = user.key;
+  // Remove the object from Algolia
+  users
+    .deleteObject(objectID)
+    .then(() => {
+      console.log('Firebase object deleted from Algolia', objectID);
+    })
+    .catch(error => {
+      console.error('Error when deleting user from Algolia', error);
+      process.exit(1);
+    });
+}
+
+
+// const userRef = database.ref('users/');
+// userRef.on('child_added', addOrUpdateUserRecord);
+// userRef.on('child_changed', addOrUpdateUserRecord);
+// userRef.on('child_removed', deleteUserRecord);
+
+// function addOrUpdateUserRecord(user) {
+//   // Get Firebase object
+//   const records = user.val();
+//   // Specify Algolia's objectID using the Firebase object key
+//   records.objectID = user.key;
+//   // Add or update object
+//   users
+//     .saveObject(records)
+//     .then(() => {
+//       console.log('Firebase object indexed in Algolia', records.objectID);
+//     })
+//     .catch(error => {
+//       console.error('Error when indexing users into Algolia', error);
+//       process.exit(1);
+//     });
+// }
+
+// function deleteUserRecord(user) {
+//   // Get Algolia's objectID from the Firebase object key
+//   const objectID = user.key;
+//   // Remove the object from Algolia
+//   users
+//     .deleteObject(objectID)
+//     .then(() => {
+//       console.log('Firebase object deleted from Algolia', objectID);
+//     })
+//     .catch(error => {
+//       console.error('Error when deleting user from Algolia', error);
+//       process.exit(1);
+//     });
+// }
---
+++ src/components/helperElements/SearchToggler.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class SearchToggler extends React.Component {
+    constructor(props){
+    super(props);
+    this.state = {
+		}
+		this.searchByHandler = this.searchByHandler.bind(this);
+	}
+
+	searchByHandler(e) {
+		this.props.searchBy(e.target.value);
+		window.indexName = e.target.value;
+		// console.log('window',window.indexName);
+	}
+
+	render() {
+		return(
+			<div>
+				<select onChange={this.searchByHandler}>
+					<option value="item">by items </option>
+					<option value="users">by users </option>
+				</select>
+			</div>
+		)
+	}
+}
