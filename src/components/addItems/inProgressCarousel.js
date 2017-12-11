2d33fca8b5d696440a54cdf754f1aa380a0d4b5c
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-12-11 15:12:26 -0500

    populate scanned items

--- src/components/addItems/inProgressCarousel.js
+++ src/components/addItems/inProgressCarousel.js
@@ -1,24 +1,63 @@
 import React from 'react';
 import Slider from 'react-slick';
 import InProgressEntry from './inProgressEntry';
+import * as firebase from 'firebase';
+import UserInfo from '../userBar/UserInfo.jsx';
+
 
 class InProgressCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inProgress: []
+    };
+    // this.handleSomething = this.handleSomething.bind(this);
+  };
+
+  componentDidMount() {
+    let collectionRef = firebase.database().ref('/items-scanned');
+
+    collectionRef.on("value", (snapshot) => {
+      let currentUID = firebase.auth().currentUser.uid;
+
+      let grabIdName = Object.keys(snapshot.val()).map((k, i) => {
+        return {
+          id: Object.keys(snapshot.val())[i],
+          name: snapshot.val()[k].name,
+          imageUrl: snapshot.val()[k].imageUrl,
+          uid: snapshot.val()[k].uid
+        }
+      })
+        .filter(collection => collection.uid.includes(currentUID));
+
+      this.setState({
+        inProgress: grabIdName
+      });
+
+    }, (error) => { console.error(error) }
+    );
+  }
+
   render() {
+    console.log('this.state', this.state)
     const settings = {
-      className: 'center',
-      infinite: false,
-      centerPadding: '0px',
-      dots: true,
+      infinite: true,
       slidesToShow: 5,
-      focusOnSelect: true,
-      afterChange: function (index) {
-        console.log(`Slider Changed to: ${index + 1}, background: #222; color: #bada55`);
-      }
+      slidesToScroll: 1,
+      dots: true,
+      speed: 500
     };
     return (
       <div className="carousel-container">
-        <Slider { ...settings }>
-          <InProgressEntry />
+      <h5>Scanned Items</h5>
+        <Slider {...settings}>
+          {
+            this.state.inProgress.map(item =>
+              <div>
+                <img src={item.imageUrl}/>
+                <h5>{item.name}</h5>
+              </div>)
+          }
         </Slider>
       </div>
     );
