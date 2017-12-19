1cca6751289e342b21b5a673fb44f71e30db14f7
Christine <christinehma214@gmail.com>
2017-12-18 19:20:05 -0500

    bottom nav

--- src/components/userBar/MyCollectionsList.jsx
+++ src/components/userBar/MyCollectionsList.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { firebaseAuth, rootRef, collection} from '../../../config/firebaseCredentials';
 import MyCollectionsEntry from './MyCollectionsEntry.jsx';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
 
 export default class MyCollectionsList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      
+    }
   }
 
+  componentWillReceiveProps() {
+  }
+  
   render() {
     return (
       <div>
-        {Object.keys(this.props.collectionList).map((id) => {
-          return <div key={id}>
-            <Link to={`/items/:${id}`} >
+        {this.props.collections ? (Object.entries(this.props.collections).map((collection) => {
+          if(collection[1] !== null) {
+          return <div key={collection} className="collectionNav">
               <MyCollectionsEntry
-                categoryId={this.props.collectionList[id].categoryId}
-                name={this.props.collectionList[id].name}
-                publicCat={this.props.collectionList[id].publicCat}
-                id={id}
+                categoryId={collection[1].categoryId}
+                name={collection[1].name}
+                publicCat={collection[1].publicCat}
+                photoUrl={collection[1].photoUrl}                
+                collectionId={collection[0]}
               />
-            </Link>
           </div>
-        })}
+        }})) : (<div/>)
+      }
       </div>
     )
   }
