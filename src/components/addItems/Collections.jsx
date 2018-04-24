abdd7682e7b59f2aa4e5e3e9362b268f9ec2ed30
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-24 19:02:18 -0400

    fixed collections

--- src/components/addItems/Collections.jsx
+++ src/components/addItems/Collections.jsx
@@ -4,54 +4,33 @@ import * as firebase from 'firebase';
 class Collections extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { collectionList: [{ id: 1, name: 'programming' }, { id: 2, name: 'comic books' }] };
+    this.state = {
+      collectionList: [{ id: 1, name: 'programming' }, { id: 2, name: 'comic books' }]
+    };
   }
-  
+
   componentDidMount() {
     //Checks database for collections owned by the user
-    let currentUser;
     let collectionRef = firebase.database().ref('/collection');
-    collectionRef.on(
-      'value',
-      snapshot => {
-        let currentUID = firebase.auth().currentUser.uid;
-        currentUser = currentUID;
-        // let currentUID = this.props.userId;
-        let grabIdName = Object.keys(snapshot.val())
-          .map((key, i) => {
-            return {
-              id: Object.keys(snapshot.val())[i],
-              name: snapshot.val()[key].name,
-              uid: snapshot.val()[key].uid,
-              value: Object.keys(snapshot.val())[i],
-              label: snapshot.val()[key].name
-            };
-          })
-          .filter(collection => collection.uid.includes(currentUID));
-        this.setState({
-          collectionList: grabIdName,
-          uid: currentUID
-        });
-        let locationRef = firebase.database().ref(`/users/${currentUID}/locations/`);
 
-        locationRef.on('value', snapshot => {
-          let grabLocations = Object.keys(snapshot.val()).map((key, i) => {
-            return {
-              lat: snapshot.val()[key].lat,
-              lng: snapshot.val()[key].lng,
-              name: key
-            };
-          });
+    collectionRef.on('value', snapshot => {
+      let grabIdName = Object.keys(snapshot.val())
+        .map((key, i) => {
+          return {
+            id: Object.keys(snapshot.val())[i],
+            name: snapshot.val()[key].name,
+            uid: snapshot.val()[key].uid,
+            value: Object.keys(snapshot.val())[i],
+            label: snapshot.val()[key].name
+          };
+        })
+        .filter(collection => collection.uid && collection.uid[0] === this.props.userId);
 
-          this.setState({
-            locationList: grabLocations
-          });
-        });
-      },
-      error => {
-        console.error(error);
-      }
-    );
+      console.log(grabIdName);
+
+      this.setState({ collectionList: grabIdName });
+    });
+  }
 
   render() {
     return (
